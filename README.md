561bb1a5b33115d7de71f083a049e49698b816e0
Akhil kumar Reddy <142980108+Octopus30@users.noreply.github.com>
2025-01-23 07:25:31 +0000

    dynamic data is uploaded

--- README.md
+++ README.md
@@ -7,4 +7,9 @@
 3. CSS
 4. Flask
 5. replit
+6. lorem ipsum --- fro text generation
+7. unsplash -- images
+8. html dog - html and css resources
+9. 
+
    
--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask,render_template,request,redirect,url_for,session,flash
 
 app = Flask(__name__)
 
+jobs = [
+    {
+        'id' : 1,
+        'Title' : 'Data Analyst',
+        'Location' : 'Bengaluru, India',
+        'Salary' : 'Rs. 10,00,000'
+    },
+    {
+        'id' : 2,
+        'Title' : 'Data Analyst',
+        'Location' : 'Bengaluru, India',
+        'Salary' : 'Rs. 10,00,000'
+    },
+    {
+        'id' : 3,
+        'Title' : 'Data Analyst',
+        'Location' : 'Bengaluru, India'
+    },{
+        'id' : 4,
+        'Title' : 'Data Analyst',
+        'Location' : 'Bengaluru, India',
+        'Salary' : 'Rs. 10,00,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",jobs = jobs)
 
 
 if __name__ == "__main__":
